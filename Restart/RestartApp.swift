c9f97285ac297a22dd4357bbb787f096b27b8791
Molindu Achintha <molinduachintha24@gmail.com>
2023-07-20 11:33:07 +0530

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Molindu Achintha on 2023-07-20.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
